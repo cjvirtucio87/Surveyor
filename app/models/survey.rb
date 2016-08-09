c5936ac3354120c0b88ff5909fe13cada4fb5b57
cjvirtucio87 <cjv287@gmail.com>
2016-08-08 19:32:29 -0700

    Added has_many associations between Survey and different question types.

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,18 @@
 class Survey < ApplicationRecord
   has_many :questions
+
+  # Access to MCQs
+  has_many :multi_choices, through: :questions,
+                           source: :questionable,
+                           source_type: 'MultiChoice'
+
+  # Access to SCQs
+  has_many :single_choices, through: :questions,
+                           source: :questionable,
+                           source_type: 'SingleChoice'
+  
+  # Access to Number Ranges
+  has_many :number_ranges, through: :questions,
+                           source: :questionable,
+                           source_type: 'NumberRange'
 end
